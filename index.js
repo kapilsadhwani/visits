4647f0ebb6927472069bb6b1bd01bbd61eafd7af
kapilsadhwani <kapil.sadhwani@spacetimeinsight.com>
2020-06-25 06:52:52 -0700

    Updated listening port

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ app.get('/', (req, res) => {
   });
 });
 
-app.listen(8081, () => {
-  console.log('Listening on port 8081');
+app.listen(8080, () => {
+  console.log('Listening on port 8080');
 });
